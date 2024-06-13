c71d209eece9ff1a8419d0972b64dbc3322bec27
1-Pankaj <pankajisrani1122334@gmail.com>
2024-06-13 11:04:48 +0530

    Fix import bugs

--- Components/Screens/Onboarding.js
+++ Components/Screens/Onboarding.js
@@ -4,10 +4,10 @@ import Stylesheet from "../StyleSheet/StyleSheet";
 
 import Codeloom from '../../assets/Vectors/Codeloom.svg'
 import Pageone from '../../assets/Vectors/pageone.svg'
-import Pagetwo from '../../assets/Vectors/pageone.svg'
-import Pagethree from '../../assets/Vectors/pageone.svg'
-import Pagefour from '../../assets/Vectors/pageone.svg'
-import Pagefive from '../../assets/Vectors/pageone.svg'
+import Pagetwo from '../../assets/Vectors/pagetwo.svg'
+import Pagethree from '../../assets/Vectors/pagethree.svg'
+import Pagefour from '../../assets/Vectors/pagefour.svg'
+import Pagefive from '../../assets/Vectors/pagefive.svg'
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Card } from "react-native-paper";
